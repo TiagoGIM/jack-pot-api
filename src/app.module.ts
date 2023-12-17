c91e35ff7a999d529e6741396de46379b09e7585
tiagoGIM <herique.sa@gmail.com>
2023-12-17 14:07:55 -0300

    feat:  creat resource ticket

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TicketController } from './ticket/ticket.controller';
+import { TicketService } from './ticket/ticket.service';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TicketModule],
+  controllers: [AppController, TicketController],
+  providers: [AppService, TicketService],
 })
 export class AppModule {}
